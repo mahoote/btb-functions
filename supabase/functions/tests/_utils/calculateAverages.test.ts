6783c0d9ec6ca8ed40fd1757a73ef8386b53ead4
mahoote <martin@teigen.biz>
2024-05-28 23:20:05 +0200

    Update version deno

--- supabase/functions/tests/_utils/calculateAverages.test.ts
+++ supabase/functions/tests/_utils/calculateAverages.test.ts
@@ -3,7 +3,7 @@ import {
     PreferenceAverages,
 } from '../../_types/gamePreferences.ts'
 import { ActivityEnum, DrunkEnum } from '../../_enums/preferencesEnum.ts'
-import { assertEquals } from 'https://deno.land/std/assert/mod.ts'
+import { assertEquals } from 'https://deno.land/std@0.224.0/assert/mod.ts'
 import calculateAverages from '../../_utils/calculateAverages.ts'
 
 Deno.test('should get averages as 1', async () => {
