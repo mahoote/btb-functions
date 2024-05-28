5b79a2237b8a2e9685187f799107fb7e679f491b
mahoote <martin@teigen.biz>
2024-05-29 00:00:27 +0200

    Create dummy players

--- supabase/functions/tests/game/create.test.ts
+++ supabase/functions/tests/game/create.test.ts
@@ -2,6 +2,7 @@ import { ActivityEnum, DrunkEnum } from '../../_enums/preferencesEnum.ts'
 import { PlayerPreference } from '../../_types/gamePreferences.ts'
 import createGame from '../../game/create.ts'
 import { assertEquals } from 'https://deno.land/std@0.224.0/assert/mod.ts'
+import { Player } from '../../_types/player.ts'
 
 Deno.test('should create a game flow', async () => {
     const response = createGame(createPlayersPreferences())
@@ -19,15 +20,34 @@ Deno.test('should create a game flow', async () => {
     })
 })
 
-function createPlayersPreferences(): PlayerPreference[] {
+function createPlayers(): Player[] {
     return [
         {
-            player_id: '1',
+            id: '1',
+            username: 'player1',
+            created_at: new Date().toISOString(),
+            is_guest: true,
+        },
+        {
+            id: '2',
+            username: 'player2',
+            created_at: new Date().toISOString(),
+            is_guest: true,
+        },
+    ]
+}
+
+function createPlayersPreferences(): PlayerPreference[] {
+    const players = createPlayers()
+
+    return [
+        {
+            player_id: players[0].id,
             drunk: DrunkEnum.WASTED,
             activity: ActivityEnum.HIGH,
         },
         {
-            player_id: '2',
+            player_id: players[1].id,
             drunk: DrunkEnum.WASTED,
             activity: ActivityEnum.HIGH,
         },
