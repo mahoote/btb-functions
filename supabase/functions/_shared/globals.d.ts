bc2267659cda660493dab7a9a7400534f3b2effe
mahoote <martin@teigen.biz>
2024-06-16 22:07:58 +0200

    Cleanup + comment

--- supabase/functions/_shared/globals.d.ts
+++ supabase/functions/_shared/globals.d.ts
@@ -1,5 +1,9 @@
 import { SupabaseClient } from 'https://esm.sh/@supabase/supabase-js@2.7.1'
 
+/**
+ * This setup allows for a globally accessible Supabase client,
+ * which simplifies client management and makes the code more modular and maintainable.
+ */
 declare global {
     var supabaseClient: SupabaseClient
 }
