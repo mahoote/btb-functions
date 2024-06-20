4434a0c0c12eaa4fb3cf273d0b31e6589344a795
mahoote <martin@teigen.biz>
2024-06-20 15:58:37 +0200

    Finish populating seed.sql

--- supabase/migrations/20240620010400_add_unique_constraint.sql
+++ supabase/migrations/20240620010400_add_unique_constraint.sql
@@ -1,8 +1,8 @@
 ALTER TABLE "public"."accessory"
-ADD CONSTRAINT accessory_name_unique UNIQUE ("name");
+ADD CONSTRAINT accessory_name_unique UNIQUE (name);
 
 ALTER TABLE "public"."game_type"
-ADD CONSTRAINT game_type_name_unique UNIQUE ("name");
+ADD CONSTRAINT game_type_name_unique UNIQUE (name);
 
 ALTER TABLE "public"."player_group_type"
-ADD CONSTRAINT player_group_type_name_unique UNIQUE ("name");
+ADD CONSTRAINT player_group_type_name_unique UNIQUE (name);
