074a92d8d2e1d63e2d1189c816d31be512f10066
mahoote <martin@teigen.biz>
2024-06-23 14:33:45 +0200

    getAveragesWithChanceForMargin - should return calculated averages with 0 and 2 as averages 🟢

--- supabase/functions/game-flow/tests/utils/gameUtils.test.ts
+++ supabase/functions/game-flow/tests/utils/gameUtils.test.ts
@@ -17,7 +17,7 @@ Deno.test('getAllCategories - should return all categories', () => {
 })
 
 Deno.test(
-    'getAveragesWithChanceForMargin - should return averages with a chance for the given margin',
+    'getAveragesWithChanceForMargin - should return calculated averages with 1 as averages',
     () => {
         const mockAverages = {
             avgDrunk: 1,
@@ -61,6 +61,54 @@ Deno.test(
         assertAlmostEquals(activityZeroPercentage, 10, tolerance)
         assertAlmostEquals(activityOnePercentage, 80, tolerance)
         assertAlmostEquals(activityTwoPercentage, 10, tolerance)
+    }
+)
+
+Deno.test(
+    'getAveragesWithChanceForMargin - should return calculated averages with 0 and 2 as averages',
+    () => {
+        const mockAverages = {
+            avgDrunk: 2,
+            avgActivity: 0,
+            drunkAvgMargin: 0.35,
+            activityAvgMargin: 0.15,
+        }
+
+        const sampleSize = 10000 // Sample size for statistical significance
+        const resultsDrunk: number[] = []
+        const resultsActivity: number[] = []
+
+        for (let i = 0; i < sampleSize; i++) {
+            const [avgDrunk, avgActivity] = getAveragesWithChanceForMargin(mockAverages)
+            resultsDrunk.push(avgDrunk)
+            resultsActivity.push(avgActivity)
+        }
+
+        const countDrunkZero = countOccurrences(resultsDrunk, 0)
+        const countDrunkOne = countOccurrences(resultsDrunk, 1)
+        const countDrunkTwo = countOccurrences(resultsDrunk, 2)
+
+        const drunkZeroPercentage = toPercentage(countDrunkZero, sampleSize)
+        const drunkOnePercentage = toPercentage(countDrunkOne, sampleSize)
+        const drunkTwoPercentage = toPercentage(countDrunkTwo, sampleSize)
+
+        const countActivityZero = countOccurrences(resultsActivity, 0)
+        const countActivityOne = countOccurrences(resultsActivity, 1)
+        const countActivityTwo = countOccurrences(resultsActivity, 2)
+
+        const activityZeroPercentage = toPercentage(countActivityZero, sampleSize)
+        const activityOnePercentage = toPercentage(countActivityOne, sampleSize)
+        const activityTwoPercentage = toPercentage(countActivityTwo, sampleSize)
+
+        const tolerance = 2 // 2% tolerance
+
+        assertAlmostEquals(drunkZeroPercentage, 0, tolerance)
+        assertAlmostEquals(drunkOnePercentage, 35, tolerance)
+        assertAlmostEquals(drunkTwoPercentage, 65, tolerance)
+
+        assertAlmostEquals(activityZeroPercentage, 85, tolerance)
+        assertAlmostEquals(activityOnePercentage, 15, tolerance)
+        assertAlmostEquals(activityTwoPercentage, 0, tolerance)
     }
 )
 
