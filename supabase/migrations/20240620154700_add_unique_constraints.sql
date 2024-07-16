663f125a9874b676f4ae058dd2c7896ec2d2fe9c
mahoote <martin@teigen.biz>
2024-07-16 14:11:49 +0200

    Squash migrations

--- supabase/migrations/20240620154700_add_unique_constraints.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "public"."game"
-ADD CONSTRAINT game_name_unique UNIQUE (name);
-
-ALTER TABLE "public"."game_has_accessory"
-ADD CONSTRAINT game_accessory_unique UNIQUE (game_id, accessory_id);
--- supabase/migrations/20240703142224_add_policy.sql
+++
@@ -1,9 +0,0 @@
-create policy "Enable read access for all users"
-on "public"."game_has_game_type"
-as permissive
-for select
-to public
-using (true);
-
-
-
