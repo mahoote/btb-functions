63e41a79185dc6b183978953abfc094cd9b49600
mahoote <martin@teigen.biz>
2024-12-01 19:36:47 +0100

    Squash migrations

--- supabase/migrations/20241017185221_add_action_card_has_buzzer.sql
+++
@@ -1,3 +0,0 @@
-alter table "public"."action_card_settings" add column "has_buzzer" boolean;
-
-
--- supabase/migrations/20241201170300_make_not_unique.sql
+++
@@ -1,4 +0,0 @@
--- Remove unique constraint from the "name" column in "game_translation"
-alter table "public"."game_translation" drop constraint if exists "game_translation_name_key";
-
-drop index if exists "public"."game_translation_name_key";
