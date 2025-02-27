972424713cee4769ec1d6331464945050e30c412
mahoote <martin@teigen.biz>
2025-02-27 03:29:55 +0100

    57 Update supabase client to allow schemas and auth

--- supabase/functions/_shared/supabaseClient.ts
+++ supabase/functions/_shared/supabaseClient.ts
@@ -1,6 +1,29 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.39.8'
 
-export const supabaseClient = createClient(
-    Deno.env.get('SUPABASE_URL') ?? '',
-    Deno.env.get('SUPABASE_ANON_KEY') ?? ''
-)
+type SupabaseSchema = 'game' | 'player'
+
+/**
+ * Create a Supabase client with the given schema and authorization.
+ * @param schema
+ * @param authorization
+ */
+export const createSupabaseClient = (schema: SupabaseSchema, authorization?: string) => {
+    // deno-lint-ignore no-explicit-any
+    const options: any = {
+        db: { schema },
+    }
+
+    if (authorization) {
+        options.global = {
+            headers: {
+                Authorization: authorization,
+            },
+        }
+    }
+
+    return createClient(
+        Deno.env.get('SUPABASE_URL') ?? '',
+        Deno.env.get('SUPABASE_ANON_KEY') ?? '',
+        options
+    )
+}
--- supabase/functions/game-flow/di.ts
+++ supabase/functions/game-flow/di.ts
@@ -1,6 +1,6 @@
 import ChallengeRepository from './repositories/challengeRepository.ts'
 import GameFlowService from './services/gameFlowService.ts'
-import { supabaseClient } from '../_shared/supabaseClient.ts'
+import { createSupabaseClient } from '../_shared/supabaseClient.ts'
 import GameRepository from './repositories/gameRepository.ts'
 import GameService from './services/gameService.ts'
 import ChallengeService from './services/challengeService.ts'
@@ -10,9 +10,10 @@ class DIContainer {
 
     public static getGameFlowService(): GameFlowService {
         if (!this.gameFlowService) {
+            const supabase = createSupabaseClient('game')
             // Repositories
-            const challengeRepository = new ChallengeRepository(supabaseClient)
-            const gameRepository = new GameRepository(supabaseClient)
+            const challengeRepository = new ChallengeRepository(supabase)
+            const gameRepository = new GameRepository(supabase)
 
             // Services
             const challengeService = new ChallengeService(challengeRepository)
